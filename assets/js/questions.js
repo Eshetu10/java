c48c4a6e2be70438274518f0b3b7997cd7d5f7c2
Eshetu10 <50382999+Eshetu10@users.noreply.github.com>
2019-09-26 16:55:10 -0400

    homework

---
+++ assets/js/questions.js
@@ -0,0 +1,53 @@
+const quizQuestions = [
+    {
+        question: "what is the beautifull stadium in the world?",
+        choices: ["Emirete stadium", "stanford bridge", "old traford", "anfield road"],
+        correctAnswer: "Emirete stadium"
+    },
+    {
+        question: "what is my favorite sport?",
+        choices: ["Soccer", "Baseball", "Footbal", "Swimming"],
+        correctAnswer: "Soccer"
+    },
+    {
+        question: "what is your favorite place to visit?",
+        choices: ["barcelona", "maldives", "Hewan Rock church", "samien maunt"],
+        correctAnswer: "Hewan Rock church"
+    },
+    {
+        question: "What is my favorite sports?",
+        choices: ["Soccer", "Baseball", "Footbal", "Swimming"],
+        correctAnswer: "Soccer"
+    },
+    {
+        question: "what is the best TV show I like?",
+        choices: ["Steve Harvey show", "little big shot", "ESPN sport soccer", "sunday suplement"],
+        correctAnswer: "ESPN sport show"
+    },
+    {
+        question: "what is the worst battle feild ever happened in human history?",
+        choices: ["the battle of Adwa", "the battle of stalingrad", "the six day war", "yom kippur"],
+        correctAnswer: "the battle of Stalingrad"
+    },
+    {
+        question: "what is the best meal of the day ?",
+        choices: ["breakfast", "lunch", "dinner", "snack"],
+        correctAnswer: "breakfast"
+    },
+    {
+        question: "what is the best book that I whould love to read?",
+        choices: ["long walk to freedom", "maths book", "football magazine", "news paper"],
+        correctAnswer: "Long walk to freedom"
+    },
+    {
+        question: "what is the best thing to do on the sutarday?",
+        choices: ["cooking", "cleaning", "sleeping", "playing sport"],
+        correctAnswer: "cleaning"
+    },
+    {
+        question: "what is the best movie of altime?",
+        choices: ["God must be crazy", "kunta kunte", "the matrix", "law and order"],
+        correctAnswer: "God must be crazy"
+    }
+ ];
+ 
